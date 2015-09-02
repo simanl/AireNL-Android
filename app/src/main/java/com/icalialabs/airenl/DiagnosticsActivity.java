08acc5a7cfe4d3f2db344d6c103d742f92a806da
Carlos Compean <ccompean@icalialabs.com>
2015-09-02 16:41:59 -0500

    first iteration finished

--- app/src/main/java/com/icalialabs/airenl/DiagnosticsActivity.java
+++ app/src/main/java/com/icalialabs/airenl/DiagnosticsActivity.java
@@ -1,5 +1,6 @@
 package com.icalialabs.airenl;
 
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,10 +8,14 @@ import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.graphics.*;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 //import com.icalialabs.airenl.blurry.Blurry;
 //import Blurry;
 import jp.wasabeef.blurry.Blurry;
@@ -20,32 +25,64 @@ public class DiagnosticsActivity extends ActionBarActivity implements ViewTreeOb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Display display = getWindowManager().getDefaultDisplay();
-//        Point size = new Point();
-//        display.getSize(size);
-//        Bitmap image = decodeSampledBitmapFromResource(getResources(),R.drawable.fondodia, size.x/4, size.y/4);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Bitmap image = decodeSampledBitmapFromResource(getResources(),R.drawable.fondodia, size.x/4, size.y/4);
 
 
         setContentView(R.layout.activity_diagnostics);
 
+        final RelativeLayout nonBoxedDiagnostics = (RelativeLayout)findViewById(R.id.nonBoxedDiagnostics);
+        nonBoxedDiagnostics.post(new Runnable() {
+            @Override
+            public void run() {
+                ViewGroup rootView = (ViewGroup) findViewById(R.id.rootDiagnosticsLayout);
+                RelativeLayout firstSectionView = (RelativeLayout) findViewById(R.id.firstSection);
+                LinearLayout.LayoutParams firstSectionParams = (LinearLayout.LayoutParams) firstSectionView.getLayoutParams();
+
+                ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
+                scrollView.setScrollY(0);
+
+                int newHeight = 0;
+                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    newHeight = rootView.getMeasuredHeight() - firstSectionParams.topMargin / 2;
+                } else {
+                    newHeight = rootView.getMeasuredHeight() - (firstSectionParams.height * 2 + firstSectionParams.topMargin + 3 * firstSectionParams.bottomMargin / 2);
+                }
+
+//                int newHeight = rootView.getMeasuredHeight() - (firstSectionParams.height + firstSectionParams.topMargin + firstSectionParams.bottomMargin / 2);
+
+                android.view.ViewGroup.LayoutParams params = nonBoxedDiagnostics.getLayoutParams();
+                params.height = newHeight;
+                nonBoxedDiagnostics.setLayoutParams(params);
+            }
+        });
 
 
         ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
         scrollView.getViewTreeObserver().addOnScrollChangedListener(this);
-//        ImageView mainViewBackground = (ImageView)findViewById(R.id.mainViewBackground);
-//        mainViewBackground.setImageBitmap(image);
+        ImageView mainViewBackground = (ImageView)findViewById(R.id.mainViewBackground);
+        mainViewBackground.setImageBitmap(image);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         ImageView background = (ImageView)findViewById(R.id.mainViewBackground);
+        ImageView blurredBackground = (ImageView) findViewById(R.id.blurImageView);
         Blurry.with(DiagnosticsActivity.this)
                 .radius(20)
                 .sampling(1)
                 .async()
-                .capture(findViewById(R.id.mainViewBackground))
-                .into((ImageView) findViewById(R.id.blurImageView));
+                .capture(background)
+                .into(blurredBackground);
+
+
+
+
+
+
     }
 
     @Override
@@ -64,7 +101,7 @@ public class DiagnosticsActivity extends ActionBarActivity implements ViewTreeOb
         final double minAlphaDarkBackground = 0.1;
         final double minAlphaBlur = 0;
         final double maxAlpahBlur = 1;
-        final double adjustmentFactor = 1 / 2.0; // reach the max alpha in the total scroll position multiplied by this factor
+        final double adjustmentFactor = 1 / 3.0; // reach the max alpha for blur in the total scroll position multiplied by this factor
 
         ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
         final int maxScroll = scrollView.getChildAt(0).getMeasuredHeight() - scrollView.getMeasuredHeight();
@@ -83,46 +120,56 @@ public class DiagnosticsActivity extends ActionBarActivity implements ViewTreeOb
 
         darkBackground.setAlpha((float)currentDarkAlpha);
         blurImageView.setAlpha((float)currentBlurAlpha);
+
+        RelativeLayout topItemsGroup = (RelativeLayout)findViewById(R.id.topItemsGroup);
+        TextView imecaQuantity = (TextView)findViewById(R.id.imecaQuantity);
+        int imecaOriginY = imecaQuantity.getTop() - currentScroll;
+        if (imecaOriginY - topItemsGroup.getBottom() <= 20) {
+            topItemsGroup.setTranslationY(-(topItemsGroup.getTop() - (imecaOriginY - (20 + topItemsGroup.getHeight()))));
+//            topItemsGroup.setTop(imecaOriginY - (20 + topItemsGroup.getHeight()));
+        } else {
+            topItemsGroup.setTranslationY(0);
+        }
     }
 
-    //    public static int calculateInSampleSize(
-//            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-//        // Raw height and width of image
-//        final int height = options.outHeight;
-//        final int width = options.outWidth;
-//        int inSampleSize = 1;
-//
-//        if (height > reqHeight || width > reqWidth) {
-//
-//            final int halfHeight = height / 2;
-//            final int halfWidth = width / 2;
-//
-//            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-//            // height and width larger than the requested height and width.
-//            while ((halfHeight / inSampleSize) > reqHeight
-//                    && (halfWidth / inSampleSize) > reqWidth) {
-//                inSampleSize *= 2;
-//            }
-//        }
-//
-//        return inSampleSize;
-//    }
-//
-//    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-//                                                         int reqWidth, int reqHeight) {
-//
-//        // First decode with inJustDecodeBounds=true to check dimensions
-//        final BitmapFactory.Options options = new BitmapFactory.Options();
-//        options.inJustDecodeBounds = true;
-//        BitmapFactory.decodeResource(res, resId, options);
-//
-//        // Calculate inSampleSize
-//        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-//
-//        // Decode bitmap with inSampleSize set
-//        options.inJustDecodeBounds = false;
-//        return BitmapFactory.decodeResource(res, resId, options);
-//    }
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
